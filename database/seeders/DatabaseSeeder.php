eda769ac145b43cdfd5bafcc0a29e6757479f54c
nabara99 <rtaufiq212@gmail.com>
2023-10-09 18:37:51 +0800

    api schedules

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,7 +21,9 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            SubjectSeeder::class,
+            ScheduleSeeder::class,
         ]);
     }
 }
